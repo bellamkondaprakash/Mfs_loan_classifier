ffb5736f9f180988159b33e37035a056894ca2ed
Prakash B <bellamkonda.da1@iiitmk.ac.in>
2020-03-13 07:46:18 -0800

    Pushed from Domino: https://try.dominodatalab.com/u/bprakash/Cust_rentention/runs/5e6ba43afb54a80006337fb3

--- model_file.py
+++ model_file.py
@@ -1,8 +1,12 @@
 import os
 
+'''
 def pickle_file(file_loc):
     model_file = [file_path for file_path in os.listdir(os.getcwd()) if file_path.endswith(".pkl")][0]
     model_filePath = os.path.join(file_loc,model_file)
     return(model_filePath)
-        
-        
+'''
+def pickle_file(current_dir_path):
+    for file_path in os.listdir(current_dir_path): 
+        if file_path.endswith(".pkl"):
+            return file_path
