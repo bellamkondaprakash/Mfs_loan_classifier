9edbdfdf420144fa3e01835420c0a230408f0fc3
Prakash B <bellamkonda.da1@iiitmk.ac.in>
2020-03-13 08:45:43 -0800

    Pushed from Domino: https://try.dominodatalab.com/u/bprakash/Cust_rentention/runs/5e6ba43afb54a80006337fb3

--- main1.py
+++ main1.py
@@ -1,7 +1,7 @@
 import os
 
 def get_model(dir_path):
-	pkl_file = [ file_path for file_path in os.listdir(os.getcwd()) if file_path.endswith('.pkl')][0]
-	model_file_path = os.path.join(dir_path,pkl_file)
-	return(model_file_path)
+    pkl_file = [ file_path for file_path in os.listdir(os.getcwd()) if file_path.endswith('.pkl')][0]
+    model_file_path = os.path.join(dir_path,pkl_file)
+    return(model_file_path)
 
