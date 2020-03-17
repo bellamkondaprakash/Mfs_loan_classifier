9054429efcaeb49cccafaaec3ff274f5d574bcc1
Prakash B <bellamkonda.da1@iiitmk.ac.in>
2020-03-16 22:02:53 -0800

    Pushed from Domino: https://try.dominodatalab.com/u/bprakash/Cust_rentention/runs/5e706707fb54a80006338326

--- api_test.py
+++
@@ -1,7 +0,0 @@
-
-
-def get_static_data(dir_path):
-    import os
-    pkl_file = [ file_path for file_path in os.listdir(os.getcwd()) if file_path.endswith('.sav')]
-    model_file_path = os.path.join(dir_path,pkl_file)
-    return(model_file_path)
