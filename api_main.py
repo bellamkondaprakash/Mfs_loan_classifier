13dbb31f9a1fd7b45e36cf819a0104b2cffe741e
Prakash B <bellamkonda.da1@iiitmk.ac.in>
2020-03-16 22:12:53 -0800

    Pushed from Domino: https://try.dominodatalab.com/u/bprakash/Cust_rentention/runs/5e706707fb54a80006338326

--- api_main.py
+++ api_main.py
@@ -2,7 +2,7 @@
 
 def api_test(args):
     import os
-    pkl_file = [ os.path.join(file_path) for file_path in os.listdir(os.getcwd()) if file_path.endswith('.sav')][0]
+    pkl_file = [ os.path.join(file_path) for file_path in os.listdir(os.getcwd()) if file_path.endswith('.sav')]
     model_file_path = os.path.join(args,os.path.basename(pkl_file))
     
     return(model_file_path)
